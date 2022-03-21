e73690091f483f68b76a6e5e496ee0c310385649
Sermil Matoto <sermil.matoto@gmail.com>
2022-03-21 20:01:36 +0800

    fix: remove unused app var

--- src/config/app.ts
+++ src/config/app.ts
@@ -1,12 +1,11 @@
 import * as functions from 'firebase-functions';
 
 // Get env variables
-const { env, host, region, okta_base_url } = functions.config().app;
+const { env, region, okta_base_url } = functions.config().app;
 const { projectId } = JSON.parse(process.env.FIREBASE_CONFIG!);
 
 const appConfig = {
   env,
-  host,
   region,
   projectId,
   okta_base_url,
